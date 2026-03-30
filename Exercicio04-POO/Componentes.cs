a4eb60fd1cf5b54a3f04c56f5e718cbd2f12be3b
Gabriel Siqueira <gabrielgit987@gmail.com>
2026-03-29 21:20:01 -0300

    classe componentes criada

---
+++ Exercicio04-POO/Componentes.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Components
+{
+	
+     public string Nome { get; set; }
+	 public decimal Preco { get; set; }
+	
+}
--- Exercicio04-POO/Form1.Designer.cs
+++ Exercicio04-POO/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtpreco = new System.Windows.Forms.TextBox();
+            this.btnadicionar = new System.Windows.Forms.Button();
+            this.lstinventario = new System.Windows.Forms.ListBox();
+            this.btnatualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome da Peça";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(204, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Preço Unitário";
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(58, 253);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(100, 20);
+            this.txtnome.TabIndex = 2;
+            // 
+            // txtpreco
+            // 
+            this.txtpreco.Location = new System.Drawing.Point(207, 253);
+            this.txtpreco.Name = "txtpreco";
+            this.txtpreco.Size = new System.Drawing.Size(100, 20);
+            this.txtpreco.TabIndex = 3;
+            // 
+            // btnadicionar
+            // 
+            this.btnadicionar.Location = new System.Drawing.Point(342, 229);
+            this.btnadicionar.Name = "btnadicionar";
+            this.btnadicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnadicionar.TabIndex = 4;
+            this.btnadicionar.Text = "Cadastrar";
+            this.btnadicionar.UseVisualStyleBackColor = true;
+            // 
+            // lstinventario
+            // 
+            this.lstinventario.FormattingEnabled = true;
+            this.lstinventario.Location = new System.Drawing.Point(46, 38);
+            this.lstinventario.Name = "lstinventario";
+            this.lstinventario.Size = new System.Drawing.Size(282, 160);
+            this.lstinventario.TabIndex = 5;
+            // 
+            // btnatualizar
+            // 
+            this.btnatualizar.Location = new System.Drawing.Point(342, 277);
+            this.btnatualizar.Name = "btnatualizar";
+            this.btnatualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnatualizar.TabIndex = 6;
+            this.btnatualizar.Text = "Atualizar";
+            this.btnatualizar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(512, 450);
+            this.Controls.Add(this.btnatualizar);
+            this.Controls.Add(this.lstinventario);
+            this.Controls.Add(this.btnadicionar);
+            this.Controls.Add(this.txtpreco);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtpreco;
+        private System.Windows.Forms.Button btnadicionar;
+        private System.Windows.Forms.ListBox lstinventario;
+        private System.Windows.Forms.Button btnatualizar;
     }
 }
 
