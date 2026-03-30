ef720dbf6da29b61464d57afa60703ddd54e309c
Gabriel Siqueira <gabrielgit987@gmail.com>
2026-03-29 23:18:21 -0300

    adicionando o arquivo da classe

---
+++ Exercicio04-POO/Componentes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio04_POO
+{   // esta classe funciona como molde para criar objetos do tipo molde
+    internal class Componentes
+    {   //o objeto tera nome
+        public string Nome { get; set; }
+        //e tambem preco/valor
+        public decimal Valor { get; set; }
+    }
+}
